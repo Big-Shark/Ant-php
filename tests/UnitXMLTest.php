bd24ea22a8b2df56493e1889f255f8bbb928b8e4
Maxim <Big_Shark@mail.ru>
2017-10-04 14:38:52 +0300

    Unit test example

--- tests/UnitXMLTest.php
+++ tests/UnitXMLTest.php
@@ -5,21 +5,30 @@ namespace tests;
 use Classes\Currency\CountCurrency;
 use Classes\Currency\Service\CbrXMLDaily;
 
+class Fake implements Classes\Currency\Service\Reader
+{
+    private $values;
+    
+    public function __construct($values)
+    {
+        $this->values = $values;
+    }
+    
+    public  function readFile() {
+        return $this->values;
+    }
+}
+    
 class UnitXMLTest extends \PHPUnit_Framework_TestCase {
 
     private $countCurrency;
 
     protected function setUp() {
-        $this->countCurrency = new CountCurrency(new CbrXMLDaily());
+        $this->countCurrency = new CountCurrency(new Fake(['AUD' => 45, 'GBP' => 50]));
     }
 
     public function testAdd() {
-        $result = $this->countCurrency->calculate(17, "Австралийский доллар", "Австралийский доллар");
-        $this->assertEquals(17, $result);
+        $result = $this->countCurrency->calculate(10, 'AUD', 'GBR');
+        $this->assertEquals(9, $result);
     }
-
-    protected function tearDown() {
-        $this->countCurrency = NULL;
-    }
-
 }
